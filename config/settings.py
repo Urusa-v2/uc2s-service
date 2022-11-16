cf98a22513dd2d830447a5654878e81fdd2dca5c
Administrator <admin@example.com>
2022-11-16 16:57:35 +0900

    chore: DEBUG False 변경

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

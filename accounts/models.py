77714a6f450cfd1a454ca3564a5c61cf17ab3af4
SooYeon Yeon <75442105+dustndus8@users.noreply.github.com>
2022-11-21 15:16:15 +0900

    feat: group 모델 생성자 추가

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class Groups(models.Model):
     name = models.TextField(unique = True)
+    def __str__(self):
+        return self.name
 
 class User(AbstractUser):
     group = models.ForeignKey(Groups,on_delete=models.CASCADE)
